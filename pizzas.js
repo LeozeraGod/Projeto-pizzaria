838820f052edbb990b834ccf0a186de14779c01b
Leonardo De Souza <116125798+LeozeraGod@users.noreply.github.com>
2023-03-06 22:17:27 -0300

    add files via upload

---
+++ pizzas.js
@@ -0,0 +1,9 @@
+let pizzaJson = [
+    {id:1, name:'Mussarela', img:'images/pizza.png', price:20.19, sizes:['100g', '530g', '860g'], description:'A Pizza de Mussarela é coberta com molho de tomate, queijo tipo mussarela, azeitonas pretas e orégano e massa com fermentação natural'},
+    {id:2, name:'Calabresa', img:'images/pizza2.png', price:18.00, sizes:['320g', '530g', '860g'], description:'É especialmente preparada com recheio generoso de linguiça calabresa de primeira qualidade, queijo muçarela e orégano'},
+    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:17.45, sizes:['320g', '530g', '860g'], description:'Pizza 4 Queijos feita com farinha de trigo, fermento em pó, molho de tomate, muçarela, provolone, gorgonzola e parmesão'},
+    {id:4, name:'Americana', img:'images/pizza4.png', price:19.77, sizes:['320g', '530g', '860g'], description:'Americana. Ingredientes: Molho de tomate, mussarela e presunto.'},
+    {id:5, name:'Sorvete', img:'images/pizza5.png', price:21.43, sizes:['320g', '530g', '860g'], description:'pizza de brigadeiro com sorvete e borda recheada de doce de leite para você servir de sobremesa e animar a festa!'},
+    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:18.55, sizes:['320g', '530g', '860g'], description:'Queijo tipo mussarela · Molho para pizza · Presunto fatiado ou ralado · Calabresa defumada fatiada · Bacom feito · Catupiry · Milho · Orégano.'},
+    {id:7, name:'Chocolate', img:'images/pizza7.png', price:22.36, sizes:['320g', '530g', '860g'], description:'Fatias de banana caramelizadas cobertas com chocolate branco; – Chocolate ao leite ou branco com lâminas de morango e leite condensado'}
+];
